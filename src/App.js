215be017c93d6b7ac26dd4f7c3d928e6fcfbfbc2
Jennie Juno <jennie@noreply.com>
2024-11-04 10:05:37 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <a href="tel:+8201023959856">전화하기</a>
+    </div>
+  );
+}
+
+export default App;
